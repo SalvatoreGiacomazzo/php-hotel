49a537965b9e885f01d4fdcc706cbbe8b43fec76
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-10-03 16:50:56 +0200

    sample table layout

--- index.php
+++ index.php
@@ -59,8 +59,50 @@ Bonus:
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 
+
 <body>
-    <h1 class="text-success">hello bootstrap</h1>
+    <div class="container w-75">
+        <table class="table table-dark">
+            <thead>
+                <tr>
+
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+            </tbody>
+        </table>
+
+
+    </div>
 </body>
 
 </html>
